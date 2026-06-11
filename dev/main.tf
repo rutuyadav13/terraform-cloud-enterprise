220d9b2733e71c6f3137d189f33fc36b4f50347c
Rutuja Yadav <Rutuja9@gmail.com>
2026-06-11 10:56:02 +0530

    Created reusable EC2 module

---
+++ dev/main.tf
@@ -0,0 +1,9 @@
+module "ec2" {
+
+  source = "../modules/ec2"
+
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  instance_name  = var.instance_name
+
+}
---
+++ dev/terraform.tfvars
@@ -0,0 +1,4 @@
+region         = "ap-south-1"
+ami_id         = "ami-0f58b397bc5c1f2e8"
+instance_type  = "t3.micro"
+instance_name  = "dev-server"
