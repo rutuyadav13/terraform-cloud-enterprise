1aa053a515a96bc4541d2880c2b0f89d12aec200
Rutuja Yadav <Rutuja9@gmail.com>
2026-06-11 10:40:55 +0530

    Added security group

--- main.tf
+++ main.tf
@@ -24,3 +24,23 @@ resource "aws_instance" "web" {
     Name = "terraform-cloud-demo"
   }
 }
+
+resource "aws_security_group" "web_sg" {
+
+  name = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
