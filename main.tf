11362370664bb28da11ebcc60a619dd61a38ac9b
Rutuja Yadav <Rutuja9@gmail.com>
2026-06-11 10:21:16 +0530

    Initial EC2 deployment

--- main.tf
+++ main.tf
@@ -1,10 +1,26 @@
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web" {
 
-  ami           = "ami-0f58b397bc5c1f2e8"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
     Name = "terraform-cloud-demo"
   }
-
 }
